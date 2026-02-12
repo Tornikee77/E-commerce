e0dd62b43e5e5dd8f75660d133b0d5345da0a5ef
Tornikee77 <tornikeesitashvili999@gmail.com>
2026-02-12 22:32:47 +0400

    feature/removing app service and controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateuserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+  @Post()
+  createUser(@Body() request: CreateuserDto) {}
+}
